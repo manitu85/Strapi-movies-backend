c6584668796e564fc7881c48028ae00dfa2fcd70
manitu85 <manitu.psy@gmail.com>
2020-07-07 00:25:55 +0200

    #

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,59 +1,13 @@
-use strict";
+'use strict';
 
-const isFirstRun = async () => {
-  const pluginStore = strapi.store({
-    environment: strapi.config.environment,
-    type: "type",
-    name: "setup",
-  });
-  const initHasRun = await pluginStore.get({ key: "initHasRun" });
-  await pluginStore.set({ key: "initHasRun", value: true });
-  return !initHasRun;
-};
+/**
+ * An asynchronous bootstrap function that runs before
+ * your application gets started.
+ *
+ * This gives you an opportunity to set up your data model,
+ * run jobs, or perform some special logic.
+ *
+ * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
+ */
 
-const setDefaultFileUploader = async () => {
-  if (strapi.config.environment !== "production") {
-    return;
-  }
-  const pluginStore = strapi.store({
-    environment: strapi.config.environment,
-    type: "plugin",
-    name: "upload",
-  });
-  const config = await pluginStore.get({ key: "provider" });
-  await pluginStore.set({
-    key: "provider",
-    value: {
-      ...config,
-      ...{
-        provider: "cloudinary",
-        name: "Cloudinary",
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_API_SECRET,
-      },
-    },
-  });
-};
-
-module.exports = async () => {
-  const shouldInit = await isFirstRun();
-  if (shouldInit) {
-    await setDefaultFileUploader();
-  }
-};
-
-
-// 'use strict';
-
-// /**
-//  * An asynchronous bootstrap function that runs before
-//  * your application gets started.
-//  *
-//  * This gives you an opportunity to set up your data model,
-//  * run jobs, or perform some special logic.
-//  *
-//  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
-//  */
-
-// module.exports = () => {};
+module.exports = () => {};
