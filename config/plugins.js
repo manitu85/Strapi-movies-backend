9ae80de174baa9f98a2f28e3122f03971f9fbef4
manitu85 <manitu.psy@gmail.com>
2020-07-07 00:10:55 +0200

    #

--- config/plugins.js
+++ config/plugins.js
@@ -2,9 +2,12 @@ module.exports = ({ env }) => ({
   upload: {
     provider: "cloudinary",
     providerOptions: {
-      cloud_name: env('strapi-upload-media-2001'),
+      cloud_name: env('manitux'),
       api_key: env('997593782972121'),
       api_secret: env('frpZY5Y32ppRkfEAHr6_7jvauLE')
     }
   },
 });
+
+
+
