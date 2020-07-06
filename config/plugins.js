48f9696d2689e17419c2770394526b0f0b8c7d00
manitu85 <manitu.psy@gmail.com>
2020-07-06 19:28:15 +0200

    added strapi-provider-upload-cloudinary

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env('strapi-upload-media-2001'),
+      api_key: env('997593782972121'),
+      api_secret: env('frpZY5Y32ppRkfEAHr6_7jvauLE')
+    }
+  },
+  // ...
+});
