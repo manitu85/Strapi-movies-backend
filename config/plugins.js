720318eeb24dc1d1a0d2ea04fe0bef182812ef06
manitu85 <manitu.psy@gmail.com>
2020-07-07 00:45:56 +0200

    #

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,32 @@
-module.exports = ({ env }) => ({
-  upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: env('manitux'),
-      api_key: env('997593782972121'),
-      api_secret: env('frpZY5Y32ppRkfEAHr6_7jvauLE')
+// module.exports = ({ env }) => ({
+//   // ...
+//   upload: {
+//     provider: "cloudinary",
+//     providerOptions: {
+//       cloud_name: env('manitux'),
+//       api_key: env('997593782972121'),
+//       api_secret: env('frpZY5Y32ppRkfEAHr6_7jvauLE')
+//     }
+//   },
+//   // ...
+// });
+
+
+module.exports = ({ env }) => {
+  if (env('NODE_ENV') === 'production') {
+    return {
+      upload: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env('manitux'),
+          api_key: env('997593782972121'),
+          api_secret: env('frpZY5Y32ppRkfEAHr6_7jvauLE')
+        }
+      },
     }
-  },
-});
-
-
-
+  // } else {
+  //   return {
+  //     module.exports = {};
+  //   }
+  }
+};
