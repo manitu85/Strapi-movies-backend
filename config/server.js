097604fbd9f76eb028a426e07b3416435132c66e
manitu85 <manitu.psy@gmail.com>
2020-07-06 03:13:22 +0200

    config env

--- config/server.js
+++ config/server.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  admin: {
+    path: '/dashboard', // We change the path to access to the admin (highly recommended for security reasons).
+  },
 });
