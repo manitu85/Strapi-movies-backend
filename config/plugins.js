4f16e24b7ed00ed6c03402bfff7196884fcfa689
manitu85 <manitu.psy@gmail.com>
2020-07-07 01:04:06 +0200

    #

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,3 @@
-// module.exports = ({ env }) => ({
-//   // ...
-//   upload: {
-//     provider: "cloudinary",
-//     providerOptions: {
-//       cloud_name: env('manitux'),
-//       api_key: env('997593782972121'),
-//       api_secret: env('frpZY5Y32ppRkfEAHr6_7jvauLE')
-//     }
-//   },
-//   // ...
-// });
-
-
 module.exports = ({ env }) => {
   if (env('NODE_ENV') === 'production') {
     return {
@@ -24,9 +10,6 @@ module.exports = ({ env }) => {
         }
       },
     }
-  // } else {
-  //   return {
-  //     module.exports = {};
-  //   }
   }
 };
+
