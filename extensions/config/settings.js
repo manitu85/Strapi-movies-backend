8d921ac88cb0dfe0aded0c515e89b0963be71353
manitu85 <manitu.psy@gmail.com>
2020-07-06 20:08:13 +0200

    #

---
+++ extensions/config/settings.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  provider: 'cloudinary',
+  providerOptions: {
+    cloud_name: 'strapi-upload-media-2001',
+    api_key: '997593782972121',
+    api_secret: 'frpZY5Y32ppRkfEAHr6_7jvauLE',
+  },
+};
