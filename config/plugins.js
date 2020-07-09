79fbbbe560ab326a7654c178514cbc0f1892c93d
manitu85 <manitu.psy@gmail.com>
2020-07-09 19:48:56 +0200

    #

--- config/plugins.js
+++ config/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => {
       upload: {
         provider: "cloudinary",
         providerOptions: {
-          cloud_name: env('manitux'),
-          api_key: env('997593782972121'),
-          api_secret: env('frpZY5Y32ppRkfEAHr6_7jvauLE')
+          cloud_name: env('CLOUDINARY_CLOUD_NAME'),
+          api_key: env('CLOUDINARY_API_KEY'),
+          api_secret: env('CLOUDINARY_API_SECRET')
         }
       },
     }
